41a32c1ebf702acd04b93d415f77fb47ea5b0662
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-01-02 22:03:12 +0530

    Add files via upload

---
+++ WEEK1/Assignment2/program5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Num5{
+	public static void main(String[] args){
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Enter the num : ");
+		int num = Sc.nextInt();
+		int count=0;
+		int temp=num;
+		if(temp==0){
+			count+=1vom;
+		}else{
+			while(temp!=0){
+				temp=temp/10;
+				count++;
+			}
+		}
+			
+		
+		System.out.println("Given number is "+count+" digit number ");
+	}
+}
