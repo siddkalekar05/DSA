a32c2fa1ecd1a086f9b20d9bdccfd662d0de1f56
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-01-03 18:36:47 +0530

    Add files via upload

--- WEEK1/Assignment2/program5.java
+++ WEEK1/Assignment2/program5.java
@@ -1,21 +1,18 @@
 import java.util.*;
-class Num5{
-	public static void main(String[] args){
-		Scanner Sc = new Scanner(System.in);
-		System.out.println("Enter the num : ");
-		int num = Sc.nextInt();
-		int count=0;
-		int temp=num;
-		if(temp==0){
-			count+=1vom;
-		}else{
-			while(temp!=0){
-				temp=temp/10;
-				count++;
-			}
-		}
-			
-		
-		System.out.println("Given number is "+count+" digit number ");
-	}
+class ArrayDemo5{
+        public static void main(String[] args){
+                Scanner Sc  = new Scanner(System.in);
+                System.out.println("Enter Array size : ");
+                int size = Sc.nextInt();
+                int arr[] = new int[size];
+                System.out.println("Enter Array elements : ");
+                for(int i=0;i<size;i++){
+                        arr[i] = Sc.nextInt();
+                }
+                int prod = 1;
+                for( int i=0;i<arr.length;i++){
+                        prod = arr[i]*prod;
+                }
+                System.out.println("product ele : "+prod);
+        }
 }
