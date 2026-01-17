f8f2e07383c7f3d977325b1be2ddc1dae1d3bb7f
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-01-17 23:10:12 +0530

    Add files via upload

---
+++ SubArray/program5.java
@@ -0,0 +1,25 @@
+//  Sum of  sub array
+import java.util.*;
+class SubDemo5{
+    static void sub(int arr[],int start,int end){
+        int sum =0;
+      for(int i=start;i<end;i++){
+        sum+=arr[i];    
+      }
+      System.err.println("Sum of ele b/w " +start+" and "+end+" is :  "+ sum);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int []{1,2,5,1,5,5,-2,1,5};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Enter start index btw 0 to "+arr.length +" ");
+        int start = sc.nextInt();
+        System.out.println("Enter end index btw 0 and "+arr.length+" ");
+        int end = sc.nextInt();
+        sub(arr,start,end);
+    }
+}
+ 
